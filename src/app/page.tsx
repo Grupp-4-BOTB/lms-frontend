6415fcc5894cfa07e4cce1dd7dffe1cb370b4780
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-05-13 20:29:13 +0200

    I have made several minor adjustments, mainly fine-tuning sizes and spacing after everyone merged their branches, since we didn't have a layout container managing the overall sizing back then.

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-[50vh]">
-      <h1 className="text-4xl font-bold">Home Page</h1>
-    </div>
-  );
-}
