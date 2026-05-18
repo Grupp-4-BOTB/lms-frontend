a354d96d09b56cfb67131a0a995ddb00c3bf99a4
Setsu <setea.music@gmail.com>
2026-05-18 10:24:07 +0200

    Add courses navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
             </Link>
 
             <Link
-              href="/dashboard"
+              href="/courses"
               className="flex items-center gap-3 p-2 rounded-l-full transition-all hover:bg-orange-50 hover:text-orange-600 group"
             >
               <Image src="/course-icon.svg" alt="" width={30} height={30} />
