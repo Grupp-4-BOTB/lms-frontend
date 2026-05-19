67c3ebcc3fa6a54268a81c836fcff56456bd5d60
Emil L <emillarsson99@gmail.com>
2026-05-19 15:47:02 +0200

    Corrected profile title

--- src/app/(dashboard)/profile/general/page.tsx
+++ src/app/(dashboard)/profile/general/page.tsx
@@ -6,12 +6,12 @@ import ProfileRouting from "@/components/ui/ProfileRouting";
 export default function ProfilePage() {
     return (
         <div className="flex flex-col ">
-            <div className="font-bold text-[45px] px-4">Team</div>
+            <div className="font-bold text-[45px] px-4">Profile</div>
             
                   <div className="w-1/2 px-2">
                     <ProfileRouting />
                   </div>
-                  
+
             <div className="flex gap-4">
                 
                 <ProfileBio />
