31823a527787ddef4f25eeec87c54cfcbdb61d01
Emil L <emillarsson99@gmail.com>
2026-06-05 15:01:04 +0200

    Removed non neccessary code

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -1,46 +1,12 @@
 "use client";
 
-// import { useState, useRef } from "react";
-
 interface ProfileCardProps {
     photoUrl: string | null;
-    // onPhotoChange: (url: string) => void;
     firstName: string;
     lastName: string;
 }
 
 export default function ProfileCard({ photoUrl, firstName, lastName }: ProfileCardProps) {
-    // const fileInputRef = useRef<HTMLInputElement>(null);
-    // const PHOTO_SERVICE_URL = "https://webapp-photoservice-emil-b7h6anhxdsamgzfx.germanywestcentral-01.azurewebsites.net";
-    // const API_KEY = process.env.NEXT_PUBLIC_API_KEY!;
-
-    // async function handlePhotoChange(event: React.ChangeEvent<HTMLInputElement>) {
-    //     const file = event.target.files?.[0];
-    //     if (file) {
-    //         // Visa preview lokalt
-    //         const url = URL.createObjectURL(file);
-    //         onPhotoChange(url);
-
-    //         const formData = new FormData();
-    //         formData.append("file", file);
-
-    //         const response = await fetch(
-    //             `${PHOTO_SERVICE_URL}/api/images/upload`,
-    //             {
-    //                 method: "POST",
-    //                 headers: {
-    //                 "X-API-Key": API_KEY
-    //                 },
-    //                 body: formData,
-    //             }
-    //         );
-    //         const data = await response.json();
-
-    //         // Byt ut lokal preview-URL mot den riktiga Blob Storage-URL:en
-    //         onPhotoChange(data.url);
-    //         console.log("Upload image URL:", data.url);
-    //     }
-    // }
     return (
         <div className="flex flex-col justify-center items-center mb-2 relative ">
             <div>
